9acb948eb1ce55da38d2969018ae22236ca1fb9a
Quang Anh Nguyễn <quanh_nguyen@outlook.com>
2018-05-24 14:49:53 +0700

    quanh-chapter1 demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+
+  # Adding a hello action to the Application controller.
+  def hello
+    render html: "hello, world!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Setting the root route.
+  root 'application#hello'
 end
